4d3c9a0e693327bddfa6783f3e8a9ceba786168e
Iker Iturralde <ikerite02@gmail.com>
2022-11-02 10:36:31 +0100

    Añadimos el metodo para pasar de pantalla

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -2,13 +2,36 @@ package com.example.fundamentals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    Button boton;
+    static String MESSAGE_NOMBRE, TAG;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        boton=findViewById(R.id.login);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                paginaSiguiente();
+            }
+        });
+    }
+
+
+
+    public void paginaSiguiente(){
+        Intent intent = new Intent(this, SingUpActivity.class); //This llama a la propia magina y se crea la otra
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/fundamentals/SingUpActivity.java
@@ -0,0 +1,4 @@
+package com.example.fundamentals;
+
+public class SingUpActivity {
+}
