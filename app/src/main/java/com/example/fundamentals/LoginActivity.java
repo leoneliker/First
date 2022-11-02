f363d66244136eee94ee2bf107319bbf511f9456
Iker Iturralde <ikerite02@gmail.com>
2022-11-02 10:52:59 +0100

    Enlace funciona

--- app/src/main/java/com/example/fundamentals/LoginActivity.java
+++ app/src/main/java/com/example/fundamentals/LoginActivity.java
@@ -8,9 +8,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
     Button boton;
+    TextView text;
     static String MESSAGE_NOMBRE, TAG;
 
 
@@ -19,8 +23,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         boton=findViewById(R.id.login);
+        text=findViewById(R.id.SingIn);
 
-        boton.setOnClickListener(new View.OnClickListener() {
+        text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 paginaSiguiente();
@@ -33,5 +38,8 @@ public class LoginActivity extends AppCompatActivity {
     public void paginaSiguiente(){
         Intent intent = new Intent(this, SingUpActivity.class); //This llama a la propia magina y se crea la otra
         startActivity(intent);
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
     }
 }
