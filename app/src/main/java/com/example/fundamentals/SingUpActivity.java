ef85e0106878601c275973e5d13834356e9c8d47
Iker Iturralde <ikerite02@gmail.com>
2022-11-03 09:46:15 +0100

    Cumulo de cosas

--- app/src/main/java/com/example/fundamentals/SingUpActivity.java
+++ app/src/main/java/com/example/fundamentals/SingUpActivity.java
@@ -2,13 +2,29 @@ package com.example.fundamentals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SingUpActivity extends AppCompatActivity {
-
+    Button boton;
+    static final String TEXTO = "RESULTADO";
+    public static final int CODIGO_NUEVO=50;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sing_up);
+        boton = findViewById(R.id.cancel);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                volver();
+            }
+        });
     }
+    public void volver(){
+        onBackPressed();
+    }
+
 }
