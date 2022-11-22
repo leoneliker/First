4e81d8645926b97ea619cdb5911d9df52463c208
Iker Iturralde <ikerite02@gmail.com>
2022-11-22 13:24:03 +0100

    Refresh

--- app/src/main/java/com/example/fundamentals/Main.java
+++ app/src/main/java/com/example/fundamentals/Main.java
@@ -1,22 +1,35 @@
 package com.example.fundamentals;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
+    private WebView miVisorWeb;
+    private SwipeRefreshLayout swipeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView click = (TextView) findViewById(R.id.click);
-        registerForContextMenu(click);
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.myswipe);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
+
+
+        WebView mycontext = (WebView) findViewById(R.id.vistaweb);
+        registerForContextMenu(mycontext);
+        //La vista dentro es un webview con permiso para zoom
+        miVisorWeb = (WebView) findViewById(R.id.vistaweb);
+        miVisorWeb.getSettings().setJavaScriptEnabled(true);
+        miVisorWeb.getSettings().setBuiltInZoomControls(true);
+        miVisorWeb.loadUrl("https://thispersondoesnotexist.com");
     }
 
 
@@ -38,5 +51,17 @@ public class Main extends AppCompatActivity {
                 return  false;
         }
     }
+    protected SwipeRefreshLayout.OnRefreshListener
+            mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+
+
+            Toast toast0 = Toast.makeText(Main.this, "Hi there! I don't exist :)", Toast.LENGTH_LONG);
+            toast0.show();
+            miVisorWeb.reload();
+            swipeLayout.setRefreshing(false);
+        }
+    };
 
 }
