9550e7cf3c3e7f688ae824b69e4eb33c7c464ed1
Iker Iturralde <ikerite02@gmail.com>
2022-11-22 12:52:31 +0100

    Terminado el main

--- app/src/main/java/com/example/fundamentals/Main.java
+++ app/src/main/java/com/example/fundamentals/Main.java
@@ -3,7 +3,11 @@ package com.example.fundamentals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main extends AppCompatActivity {
     @Override
@@ -14,4 +18,25 @@ public class Main extends AppCompatActivity {
         TextView click = (TextView) findViewById(R.id.click);
         registerForContextMenu(click);
     }
+
+
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+    }
+
+    public boolean onContextItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast toast = Toast.makeText(this, "Item copied", Toast.LENGTH_LONG);
+                toast.show();
+                return true;
+            case R.id.item2:
+                Toast toast2 = Toast.makeText(this, "Item downloaded", Toast.LENGTH_LONG);
+                toast2.show();
+                return true;
+            default:
+                return  false;
+        }
+    }
+
 }
